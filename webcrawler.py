ec63d7fcd8c9c1cc4010e9e3525f9a7252fd539b
Nick <nickcica@gmail.com>
2014-11-06 22:53:31 +0900

    Add crawl_web function

--- webcrawler.py
+++ webcrawler.py
@@ -1,5 +1,14 @@
+# WebCrawler from Udacity CS101
+
+def get_page(url):
+    try:
+        import urllib
+        return urllib.urlopen(url).read()
+    except:
+        return ""
+
 def get_next_target(page):
-    start_link = page.find('a href=')
+    start_link = page.find('<a href=')
     if start_link == -1:
         return None, 0
     start_quote = page.find('"', start_link)
@@ -7,21 +16,33 @@ def get_next_target(page):
     url = page[start_quote + 1:end_quote]
     return url, end_quote
     
-def print_all_links(page):
+def union(p,q):
+    for e in q:
+        if e not in p:
+            p.append(e)
+    
+def get_all_links(page):
+    links = []
     while True:
         url, endpos = get_next_target(page)
         if url:
-            print url
+            links.append(url)
             page = page[endpos:]
         else:
             break
+    return links
+        
+def crawl_web(seed):
+    toCrawl = [seed]
+    crawled = []
+    while toCrawl:
+        page = toCrawl.pop() # Depth First Search
+        if page not in crawled:
+            union(toCrawl,get_all_links(get_page(page)))
+            crawled.append(page)
+    return crawled
             
-def get_page(url):
-    try:
-        import urllib
-        return urllib.urlopen(url).read()
-    except:
-        return ""
-            
-#links = print_all_links(get_page('http://xkcd.com/353/'))
+#links = get_all_links(get_page('http://www.udacity.com/cs101x/index.html'))
 #print links
+seed = 'http://www.udacity.com/cs101x/index.html'
+print crawl_web(seed)
