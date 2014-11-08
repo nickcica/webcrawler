e34c43d7d7bd48fd03fef34ee5bff1623c60c5f1
Nick <nickcica@gmail.com>
2014-11-08 23:19:17 +0900

    Add add_page_to_index and look_up function

--- webcrawler.py
+++ webcrawler.py
@@ -1,5 +1,7 @@
 # WebCrawler from Udacity CS101
 
+index = [] 
+
 def get_page(url):
     try:
         import urllib
@@ -35,14 +37,51 @@ def get_all_links(page):
 def crawl_web(seed):
     toCrawl = [seed]
     crawled = []
+    index = []
     while toCrawl:
         page = toCrawl.pop() # Depth First Search
         if page not in crawled:
-            union(toCrawl,get_all_links(get_page(page)))
+            content = get_page(page)
+            add_page_to_index(index, page, content)
+            union(toCrawl,get_all_links(content))
             crawled.append(page)
-    return crawled
-            
-#links = get_all_links(get_page('http://www.udacity.com/cs101x/index.html'))
-#print links
-seed = 'http://www.udacity.com/cs101x/index.html'
-print crawl_web(seed)
+    return index
+
+def add_to_index(index, keyword, url): # --> Done
+    for entry in index:
+        if entry[0] == keyword: 
+            # prevent duplicate url listings
+            if not url in entry[1]:
+                entry[1].append(url)
+                return
+    # not found, add new keyword to index
+    index.append([keyword,[url]])
+    
+# Test --> Pass        
+#add_to_index(index, 'udacity', 'http://www.udacity.com')
+#add_to_index(index, 'computing', 'http://www.amc.org')
+#add_to_index(index, 'udacity', 'http://www.npr.org')
+#print index
+
+def look_up(index, keyword): # --> Done
+    for entry in index:
+        if entry[0] == keyword:
+            return entry[1]
+    return []
+
+def add_page_to_index(index, url, content): # --> Done!
+    keywords = content.split()
+    for keyword in keywords:
+        add_to_index(index, keyword, url)
+        
+# Test --> Pass
+#add_page_to_index(index, 'fake.test', "This is a test")
+#add_page_to_index(index, 'real.test', "This is not a test")
+#print index
+#print look_up(index, 'is')
+        
+# Test --> Fail
+#seed = 'http://www.udacity.com/cs101x/index.html'
+#print crawl_web(seed)
+#print index
+#print look_up(index, 'good')
